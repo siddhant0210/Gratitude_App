d32fc0d889b60bdc2ef790eafc2e38bb86e8ea5e
siddhant0210 <siddwivedi0210@gmail.com>
2024-10-25 12:53:31 +0530

    Added random color function in thoughts fragment

--- app/src/main/java/com/example/gratitude/fragments/ThoughtsFragment.kt
+++ app/src/main/java/com/example/gratitude/fragments/ThoughtsFragment.kt
@@ -2,14 +2,17 @@ package com.example.gratitude.fragments
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.res.ResourcesCompat.getColor
 import com.example.gratitude.R
 import com.example.gratitude.activities.LandingActivity
 import com.example.gratitude.databinding.FragmentThoughtsBinding
@@ -18,16 +21,19 @@ import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.util.Date
 import java.util.Locale
+import java.util.Random
 
 class ThoughtsFragment : Fragment() {
 
     private lateinit var binding: FragmentThoughtsBinding
-
+    private var lastColor: String? = null
+    private var newColor: String? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentThoughtsBinding.inflate(inflater, container, false)
+        setRandomBackgroundColor()
         return binding.root
     }
 
@@ -48,7 +54,7 @@ class ThoughtsFragment : Fragment() {
         challengeIcon.visibility = View.GONE
         val toolbar = activity.findViewById<Toolbar>(R.id.toolbar)
         toolbar.setNavigationIcon(R.drawable.baseline_arrow_back_24)
-        toolbar.setBackgroundColor(resources.getColor(R.color.blue))
+        toolbar.setBackgroundColor(Color.parseColor(newColor))
 
         toolbar.setNavigationOnClickListener {
             val intent = Intent(requireActivity(), LandingActivity::class.java)
@@ -58,11 +64,33 @@ class ThoughtsFragment : Fragment() {
         }
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint("SetTextI18n", "ResourceAsColor")
     private fun setTodayDate() {
         val dateFormat = SimpleDateFormat("dd MMM, yyyy", Locale.getDefault())
         val today = dateFormat.format(Date())
         binding.date.text = "Today: $today"
     }
+
+    // Function to generate a random hex color
+    private fun generateRandomHexColor(): String {
+        val random = Random()
+        val color = String.format("#%06X", random.nextInt(0xFFFFFF + 1))
+        return color
+    }
+
+    private fun setRandomBackgroundColor() {
+        do {
+            newColor = generateRandomHexColor()
+        } while (newColor == lastColor) // It should change everytime
+
+        lastColor = newColor
+        // Set the background color
+        binding.root.setBackgroundColor(Color.parseColor(newColor))
+        binding.checkIcon.setBackgroundColor(Color.parseColor((newColor)))
+
+        val window: Window = requireActivity().window
+        window.statusBarColor = Color.parseColor(newColor)
+    }
+
 
 }
